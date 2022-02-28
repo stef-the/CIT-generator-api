3e4cc4738599b0de1a9393a6de576143d3cf3263
stef-the <stefthedoggo4@gmail.com>
2022-02-28 09:05:15 -0800

    :thumbsup:

--- pages/api/items/duplicates.ts
+++ pages/api/items/duplicates.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import * as fs from "fs";
-import internal from "stream";
+import absoluteUrl from "next-absolute-url";
 
 type Data = {
 	status: number;
@@ -12,53 +12,63 @@ export default function handler(
 	req: NextApiRequest,
 	res: NextApiResponse<Data>
 ) {
-    function displayRename(input:string) {
-		let output = '';
-		('§a' + input).split('§').forEach((part) => {
-			output += part.substring(1);
-		});
-		return output;
-	}
+	const { origin } = absoluteUrl(req);
+    function displayRename(input: string) {
+        let output = "";
+        ("§a" + input).split("§").forEach((part) => {
+            output += part.substring(1);
+        });
+        return output;
+    }
 
-	const RAWitems = fs.readdirSync("NotEnoughUpdates-REPO/items");
-	let items: String[] = [];
-    let duplicates: String[] = [];
-	let data: any[] = [];
-    let increment: number = 0;
+	if (origin.includes("//localhost:")) {
+		const RAWitems = fs.readdirSync("NotEnoughUpdates-REPO/items");
+		let items: String[] = [];
+		let duplicates: String[] = [];
+		let data: any[] = [];
+		let increment: number = 0;
 
-	for (const item of RAWitems) {
-		items.push(item.substr(0, item.length - 5));
-		data.push(
-			JSON.parse(
-				fs.readFileSync(`NotEnoughUpdates-REPO/items/${item}`, {
-					encoding: "utf8",
-					flag: "r",
-				})
-			)
+		for (const item of RAWitems) {
+			items.push(item.substr(0, item.length - 5));
+			data.push(
+				JSON.parse(
+					fs.readFileSync(`NotEnoughUpdates-REPO/items/${item}`, {
+						encoding: "utf8",
+						flag: "r",
+					})
+				)
+			);
+		}
+
+		for (const item of data) {
+			for (const part of data) {
+				//@ts-ignore
+				if (
+					displayRename(item["displayname"]) ==
+					displayRename(part["displayname"])
+				) {
+					increment = increment + 1;
+					if (increment > 1) {
+						duplicates.push(displayRename(item["displayname"]));
+					}
+				}
+			}
+			increment = 0;
+		}
+
+		const output = Array.from(new Set(duplicates).values());
+
+		fs.writeFileSync(
+			"./pages/api/items/duplicates.json",
+			JSON.stringify({
+				data: output,
+				timestamp: Date.now(),
+			})
 		);
 	}
 
-    for (const item of data) {
-        for (const part of data) { //@ts-ignore
-            if (displayRename(item['displayname']) == displayRename(part['displayname'])) {
-                increment = increment + 1;
-                if (increment > 1) {
-                    duplicates.push(displayRename(item['displayname']))
-                }
-            }
-        }
-        increment = 0
-    }
-
-    const output = Array.from(new Set(duplicates).values())
-
-    fs.writeFileSync('./pages/api/items/duplicates.json', JSON.stringify({
-        data: output,
-        timestamp:
-    }))
-
-	res.status(200).json({ 
-		status: 200,
-		data: output
+	res.status(200).json({
+		status: 200, //@ts-ignore
+		data: JSON.parse(fs.readFileSync("./pages/api/items/duplicates.json")),
 	});
 }
--- pages/api/items/index.ts
+++ pages/api/items/index.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import * as fs from "fs";
-import internal from "stream";
+import absoluteUrl from "next-absolute-url";
 
 type Data = {
 	status: number;
@@ -12,24 +12,36 @@ export default function handler(
 	req: NextApiRequest,
 	res: NextApiResponse<Data>
 ) {
-	const RAWitems = fs.readdirSync("NotEnoughUpdates-REPO/items");
-	let items: String[] = [];
-	let data: any[] = [];
+	const { origin } = absoluteUrl(req);
 
-	for (const item of RAWitems) {
-		items.push(item.substr(0, item.length - 5));
-		data.push(
-			JSON.parse(
-				fs.readFileSync(`NotEnoughUpdates-REPO/items/${item}`, {
-					encoding: "utf8",
-					flag: "r",
-				})
-			)
+	if (origin.includes("//localhost:")) {
+		const RAWitems = fs.readdirSync("NotEnoughUpdates-REPO/items");
+		let items: String[] = [];
+		let data: any[] = [];
+
+		for (const item of RAWitems) {
+			items.push(item.substr(0, item.length - 5));
+			data.push(
+				JSON.parse(
+					fs.readFileSync(`NotEnoughUpdates-REPO/items/${item}`, {
+						encoding: "utf8",
+						flag: "r",
+					})
+				)
+			);
+		}
+
+		fs.writeFileSync(
+			"./pages/api/items/main.json",
+			JSON.stringify({
+				data: data,
+				timestamp: Date.now(),
+			})
 		);
 	}
 
 	res.status(200).json({ 
-		status: 200,
-		data: data
+		status: 200, //@ts-ignore
+		data: JSON.parse(fs.readFileSync("./pages/api/items/main.json"))
 	});
 }
