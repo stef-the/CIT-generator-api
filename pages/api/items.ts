c870427667976cd9532357ed69eb03e7c351f3b2
stef-the <stefthedoggo4@gmail.com>
2022-02-27 20:59:27 -0800

    Update items.ts

--- pages/api/items.ts
+++ pages/api/items.ts
@@ -10,24 +10,21 @@ export default function handler(
 	req: NextApiRequest,
 	res: NextApiResponse<Data>
 ) {
-	if (req.method === "GET") {
-		const RAWitems = fs.readdirSync("NotEnoughUpdates-REPO/items");
-		let items: String[] = [];
-		let data: JSON[] = [];
+	const RAWitems = fs.readdirSync("NotEnoughUpdates-REPO/items");
+	let items: String[] = [];
+	let data: JSON[] = [];
 
-		for (const item of RAWitems) {
-			items.push(item.substr(0, item.length - 5));
-			data.push(
-				JSON.parse(
-					fs.readFileSync(`NotEnoughUpdates-REPO/items/${item}`, {
-						encoding: "utf8",
-						flag: "r",
-					})
-				)
-			);
-		}
-
-		res.status(200).json({ data: data });
-	} else {
+	for (const item of RAWitems) {
+		items.push(item.substr(0, item.length - 5));
+		data.push(
+			JSON.parse(
+				fs.readFileSync(`NotEnoughUpdates-REPO/items/${item}`, {
+					encoding: "utf8",
+					flag: "r",
+				})
+			)
+		);
 	}
+
+	res.status(200).json({ data: data });
 }
