b9e2aa164406a270197cd001297a7b18f415226f
stef-the <stefthedoggo4@gmail.com>
2022-02-27 20:54:49 -0800

    YOOOO

--- pages/api/items.ts
+++ pages/api/items.ts
@@ -1,9 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import * as fs from 'fs';
+import * as fs from "fs";
 
 type Data = {
-	name: string;
+	data: JSON<any>;
 };
 
 export default function handler(
@@ -11,7 +11,23 @@ export default function handler(
 	res: NextApiResponse<Data>
 ) {
 	if (req.method === "GET") {
-		res.status(200).json({ name: "hi" });
+		const RAWitems = fs.readdirSync("NotEnoughUpdates-REPO/items");
+		let items: String[] = [];
+		let data: JSON[] = [];
+
+		for (const item of RAWitems) {
+			items.push(item.substr(0, item.length - 5));
+			data.push(
+				JSON.parse(
+					fs.readFileSync(`NotEnoughUpdates-REPO/items/${item}`, {
+						encoding: "utf8",
+						flag: "r",
+					})
+				)
+			);
+		}
+
+		res.status(200).json({ test: data });
 	} else {
 	}
 }
